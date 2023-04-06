0ee7901a3c2c84a3d7f566dfe14194dcde2dbafe
Mutesi <ktesialine@gmail.com>
2023-04-06 16:00:44 +0200

    build:fix url for top headiline articles

--- src/features/api.js
+++ src/features/api.js
@@ -3,16 +3,16 @@ const API_KEY = "2868fed31c4d4eccbc9ecbea3f12b80d";
 export const getTopHeadlines = async (country, sources = null) => {
   const url = sources
     ? `https://news-proxy.netlify.app/api/top-headlines?sources=${sources}&apiKey=${API_KEY}`
-    : `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
+    : `https://news-proxy.netlify.app/top-headlines?country=${country}&apiKey=${API_KEY}`;
 
-  const response = await fetch(url, { mode: "cors" });
+  const response = await fetch(url);
   const data = await response.json();
   return data;
 };
 
 export const getSources = async () => {
   const url = `https://news-proxy.netlify.app/api/top-headlines/sources?apiKey=${API_KEY}`;
-  const response = await fetch(url, { mode: "cors" });
+  const response = await fetch(url);
   const data = await response.json();
   const publishers = data.sources.map((source) => source.id);
   return publishers;
@@ -20,7 +20,7 @@ export const getSources = async () => {
 
 export const getEverything = async () => {
   const url = `https://news-proxy.netlify.app/api/everything?q=everything&apiKey=${API_KEY}`;
-  const response = await fetch(url, { mode: "cors" });
+  const response = await fetch(url);
   const data = await response.json();
   return data;
 };
