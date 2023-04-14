ccfcf06ebbc00cba7d54e466f12fc24abc2af51b
Mutesi <ktesialine@gmail.com>
2023-04-14 11:54:38 +0200

    fix:env variable

--- src/features/api.js
+++ src/features/api.js
@@ -1,27 +1,42 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 const API_URL = "https://news-proxy.netlify.app/api";
 
 export const getTopHeadlines = async (country, sources = null) => {
-  const url = sources
-    ? `${API_URL}/top-headlines?sources=${sources}&apiKey=${API_KEY}&pageSize=10`
-    : `${API_URL}/top-headlines?country=${country}&apiKey=${API_KEY}&pageSize=10`;
+  try {
+    const url = sources
+      ? `${API_URL}/top-headlines?sources=${sources}&apiKey=${API_KEY}&pageSize=10`
+      : `${API_URL}/top-headlines?country=${country}&apiKey=${API_KEY}&pageSize=10`;
 
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return { message: "An error occurred. Please try again later." };
+  }
 };
 
 export const getSources = async () => {
-  const url = `${API_URL}/top-headlines/sources?apiKey=${API_KEY}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  const publishers = data.sources.map((source) => source.id);
-  return publishers;
+  try {
+    const url = `${API_URL}/top-headlines/sources?apiKey=${API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const publishers = data.sources.map((source) => source.id);
+    return publishers;
+  } catch (error) {
+    console.error(error);
+    return { message: "An error occurred. Please try again later." };
+  }
 };
 
 export const getEverything = async (value) => {
-  const url = `${API_URL}/everything?q=${value}&apiKey=${API_KEY}&pageSize=20`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  try {
+    const url = `${API_URL}/everything?q=${value}&apiKey=${API_KEY}&pageSize=20`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return { message: "An error occurred. Please try again later." };
+  }
 };
