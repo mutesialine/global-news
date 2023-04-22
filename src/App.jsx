9dc7a63b6e8b3a6d7917df657303b8b231f806b8
Mutesi <ktesialine@gmail.com>
2023-04-22 11:56:36 +0200

    fix:remove an unnecessary empty fragment

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import FullArticle from "./pages/FullArticle";
 import NewsArticles from "./pages/NewsArticles";
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<NewsArticles />} />
-          <Route path="/fullArticle" element={<FullArticle />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NewsArticles />} />
+        <Route path="/fullArticle" element={<FullArticle />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default App;
